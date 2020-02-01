087a2453db0b3537f1ad582e0df1f78594dbe48c
Manu <espesito16@gmail.com>
2020-01-31 17:21:22 -0800

    First Commit

---
+++ InnocvPrueba/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InnocvPrueba
+//
+//  Created by Manu Espeso on 31/01/2020.
+//  Copyright © 2020 Manu Espeso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
